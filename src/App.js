25dc0cea0142fed1f4c17f9fc730d7810db6757b
Benson Maruchu <benmaruchu@gmail.com>
2018-07-27 12:48:32 +0300

    Fix #1: Add HRM support

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from 'react';
 import './App.css';
 import logo from './logo.svg';
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">
-          Welcome to React
+          Welcome to React here
         </h1>
       </header>
       <p className="App-intro">
-        To get started, edit
+        Make magic Happen here
         {' '}
         <code>
           src/App.js
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const render = (Component) => { ReactDOM.render(<Component />, document.getElementById('root')); };
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default; // eslint-disable-line  global-require
+    render(NextApp);
+  });
+}
