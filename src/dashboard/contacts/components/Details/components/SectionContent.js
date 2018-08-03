158158df372678397d82c295f9c7c61dfd9b522d
Benson Maruchu <benmaruchu@gmail.com>
2018-08-03 18:28:25 +0300

    Contact Details: Remove padding top & bottom  for section content

--- src/dashboard/contacts/components/Details/components/SectionContent.js
+++ src/dashboard/contacts/components/Details/components/SectionContent.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function SectionContent({ children }) {
   return (
-    <Col span={24} style={{ padding: '30px' }}>
+    <Col span={24} style={{ padding: '0 30px' }}>
       {children}
     </Col>
   );
