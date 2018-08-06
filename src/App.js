69242ac0c92049f5126ff925093efc493e60d848
Benson Maruchu <benmaruchu@gmail.com>
2018-08-06 10:17:02 +0300

    Routing: Configure initial routes for contacts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Dashboard from './dashboard';
 // import global styles
 import './utils.css';
 
 
 function App() {
-  return (<Dashboard />);
+  return (
+    <BrowserRouter>
+      <Dashboard />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,6 +1,7 @@
 import { Icon, Layout, Menu } from 'antd';
 import classnames from 'classnames';
 import React from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
 // Components
 import Contacts from './contacts';
 // import styles
@@ -27,16 +28,20 @@ export default function Dashboard() {
         <div className="logo" />
         <Menu mode="inline">
           <Menu.Item key="1">
-            <Icon type="user" />
-            <span>
-              Contacts
-            </span>
+            <NavLink to="/contacts">
+              <Icon type="user" />
+              <span>
+                Contacts
+              </span>
+            </NavLink>
           </Menu.Item>
         </Menu>
       </Sider>
       <Layout>
         <Content className={cx('content')}>
-          <Contacts />
+          <Switch>
+            <Route path="/contacts" exact component={Contacts} />
+          </Switch>
         </Content>
       </Layout>
     </Layout>
