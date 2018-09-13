28f7f8053916c72ed6ed492e5650dc07639de644
Benson Maruchu <benmaruchu@gmail.com>
2018-09-14 00:59:27 +0300

    Plan: Add initial plan UIs without functionality
    This add only layouts for
    - Plans
    - Phases
    - Action

---
+++ src/dashboard/plans/components/Phases/components/Phase/components/ActionCard/index.js
@@ -0,0 +1,104 @@
+import { Badge, Card, Col, Icon, Row } from 'antd';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+/* local constants */
+const { Meta } = Card;
+
+/**
+ * Action Detail component
+ *
+ * @function
+ * @name Details
+ * @param {Object} props
+ * @param {string} props.name
+ * @param {string} props.incident
+ * @param {number} props.taskCount
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+function Details({ name, incident, taskCount }) {
+  return (
+    <Row justify="space-between">
+      <Col span={21}>
+        <h3 title={name}>{name}</h3>
+        <p style={{ fontSize: '11px', color: '#909090' }}>{incident}</p>
+      </Col>
+      <Col span={2}>
+        <Badge
+          count={taskCount}
+          style={{
+            backgroundColor: '#fff',
+            color: '#999',
+            boxShadow: '0 0 0 1px #d9d9d9 inset',
+          }}
+        />
+        <p style={{ fontSize: '11px', marginTop: '5px' }}>Tasks</p>
+      </Col>
+    </Row>
+  );
+}
+
+/**
+ * Action card component
+ *
+ * @function
+ * @name Action
+ * @param {Object} props
+ * @param {string} props.name
+ * @param {string} props.incident
+ * @param {number} props.taskCount
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+export default function ActionCard({ name, incident, taskCount }) {
+  return (
+    <Card
+      style={{
+        width: '90%',
+        margin: '20px auto',
+        borderLeft: '3px solid #0092fd',
+      }}
+      actions={[
+        <Link to="/plans/action">
+          <Icon type="eye" title="Explore" />
+        </Link>,
+        <Icon type="edit" title="Edit" />,
+        <Icon type="hdd" title="Archive" />,
+      ]}
+    >
+      <Meta
+        title={
+          <Details name={name} incident={incident} taskCount={taskCount} />
+        }
+      />
+    </Card>
+  );
+}
+
+/* default Action props */
+ActionCard.defaultProps = {
+  taskCount: 0,
+};
+
+/* props validations for Action */
+ActionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  incident: PropTypes.string.isRequired,
+  taskCount: PropTypes.number,
+};
+
+/* default Details props */
+Details.defaultProps = {
+  taskCount: 0,
+};
+
+/* props validation for details */
+Details.propTypes = {
+  name: PropTypes.string.isRequired,
+  incident: PropTypes.string.isRequired,
+  taskCount: PropTypes.number,
+};
---
+++ src/dashboard/plans/components/PlanList/components/PlanCard.js
@@ -0,0 +1,122 @@
+import { Badge, Card, Col, Icon, Row } from 'antd';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+/* local constants */
+const { Meta } = Card;
+
+/**
+ * Plan Detail component
+ *
+ * @function
+ * @name Details
+ * @param {Object} props
+ * @param {string} props.name
+ * @param {string} props.incident
+ * @param {number} props.activityCount
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+function Details({ name, jurisdiction, activityCount, updatedAt }) {
+  return (
+    <React.Fragment>
+      <Row justify="space-between">
+        <Col span={21}>
+          <h3 title={name}>{name}</h3>
+          <p style={{ fontSize: '11px', color: '#909090' }}>{jurisdiction}</p>
+        </Col>
+        <Col span={2}>
+          <Badge
+            count={activityCount}
+            style={{
+              backgroundColor: '#fff',
+              color: '#999',
+              boxShadow: '0 0 0 1px #d9d9d9 inset',
+            }}
+          />
+          <p style={{ fontSize: '11px', marginTop: '5px' }}>Actions</p>
+        </Col>
+      </Row>
+      <p style={{ fontSize: '11px', color: '#909090' }}>
+        Last updated Date: {new Intl.DateTimeFormat('en-GB').format(updatedAt)}
+      </p>
+    </React.Fragment>
+  );
+}
+
+/**
+ * Plan card component
+ *
+ * @function
+ * @name Plan
+ * @param {Object} props
+ * @param {string} props.name
+ * @param {string} props.incident
+ * @param {number} props.activityCount
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+export default function PlanCard({
+  name,
+  jurisdiction,
+  activityCount,
+  createdAt,
+  updatedAt,
+}) {
+  return (
+    <Card
+      style={{
+        width: '90%',
+        margin: '20px auto',
+        borderLeft: '3px solid #0092fd',
+      }}
+      actions={[
+        <Link to="/plans/phases">
+          <Icon type="eye" title="Explore" />
+        </Link>,
+        <Icon type="edit" title="Edit" />,
+        <Icon type="hdd" title="Archive" />,
+      ]}
+    >
+      <Meta
+        title={
+          <Details
+  name={name}
+  jurisdiction={jurisdiction}
+  activityCount={activityCount}
+  updatedAt={updatedAt}
+/>
+        }
+      />
+    </Card>
+  );
+}
+
+/* default Plan props */
+PlanCard.defaultProps = {
+  activityCount: 0,
+};
+
+/* props validations for Plan */
+PlanCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  jurisdiction: PropTypes.string.isRequired,
+  activityCount: PropTypes.number,
+  updatedAt: PropTypes.instanceOf(Date).isRequired,
+};
+
+/* default Details props */
+Details.defaultProps = {
+  activityCount: 0,
+};
+
+/* props validation for details */
+Details.propTypes = {
+  name: PropTypes.string.isRequired,
+  jurisdiction: PropTypes.string.isRequired,
+  activityCount: PropTypes.number,
+  updatedAt: PropTypes.instanceOf(Date).isRequired,
+};
