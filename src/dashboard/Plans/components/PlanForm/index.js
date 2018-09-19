ca22cbbb69719fee79b8beebfa1f15937f051758
Benson Maruchu <benmaruchu@gmail.com>
2018-09-19 12:23:17 +0300

    Plans: Add create plan form and refactor code
    This cleanup the plan list UI by removing export button and also add
    form for creating new plan.

---
+++ src/dashboard/Plans/components/PlanForm/index.js
@@ -0,0 +1,98 @@
+import { Button, Col, Form, Row } from 'antd';
+import React, { Component } from 'react';
+import Select from '../../../../common/components/Select';
+
+/* local constants */
+const FormItem = Form.Item;
+
+// fake data
+const incidentTypes = [
+  { label: 'Flood', value: 'flood' },
+  { label: 'Earthquake', value: 'earthquake' },
+  { label: 'Fire', value: 'fire' },
+  { label: 'Cholera', value: 'cholera' },
+  { label: 'Tornado', value: 'tornado' },
+];
+const locations = [
+  { label: 'Dar es salaam', value: 'dar es salaam' },
+  { label: 'Mwanza', value: 'mwanza' },
+  { label: 'Iringa', value: 'iringa' },
+  { label: 'Temeke', value: 'temeke' },
+  { label: 'Arusha', value: 'arusha' },
+];
+
+/**
+ * Form for creating and editing plan
+ *
+ * @extends React.Component
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+export default class PlanForm extends Component {
+  handleSubmit = e => {
+    // todo handle form submission
+  };
+
+  render() {
+    /* layout for formItem */
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+    /* layout for form action buttons */
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    };
+
+    const { onCancel } = this.props;
+
+    return (
+      <Form
+        onSubmit={this.handleSubmit}
+        style={{ maxWidth: '400px', margin: '0 auto' }}
+      >
+        {/* incident type select input */}
+        <FormItem label="Incident Type" {...formItemLayout}>
+          <Select
+            options={incidentTypes}
+            placeholder="Select Incident Type ..."
+          />
+        </FormItem>
+        {/* end incident type select input */}
+        {/* location select input */}
+        <FormItem label="Location" {...formItemLayout}>
+          <Select options={locations} placeholder="Select Location ..." />
+        </FormItem>
+        {/* end location select input */}
+        {/* form action buttons */}
+        <FormItem {...tailFormItemLayout}>
+          <Row justify="space-between">
+            <Col span={10}>
+              <Button onClick={onCancel}>Cancel</Button>
+            </Col>
+            <Col span={10}>
+              <Button type="primary">Save</Button>
+            </Col>
+          </Row>
+        </FormItem>
+        {/* end form action buttons */}
+      </Form>
+    );
+  }
+}
--- src/dashboard/Plans/components/PlanList/index.js
+++ src/dashboard/Plans/components/PlanList/index.js
@@ -1,6 +1,7 @@
-import { Button, Col, Dropdown, Icon, Layout, List, Menu, Row } from 'antd';
-import React from 'react';
+import { Button, Col, Drawer, Icon, Layout, List, Menu, Row } from 'antd';
+import React, { Component } from 'react';
 import Select from '../../../../common/components/Select';
+import PlanForm from '../PlanForm';
 import PlanCard from './components/PlanCard';
 
 /* local constants */
@@ -78,84 +79,104 @@ const options = [
  * @version 0.1.0
  * @since 0.1.0
  */
-export default function PlanList() {
-  return (
-    <Layout>
-      {/* start primary header */}
-      <Header
-        style={{
-          background: '#fff',
-          paddingLeft: 10,
-          borderBottom: '1px solid #E0E0E0',
-          flexBasis: '50px',
-        }}
-      >
-        <h3>Plans</h3>
-      </Header>
-      {/* end primary header */}
-      {/* Toolbar */}
-      {/* start secondary header */}
-      <Header
-        style={{
-          background: '#fff',
-          paddingLeft: 10,
-          borderBottom: '1px solid #E0E0E0',
-          flexBasis: '50px',
-        }}
-      >
-        <Row justify="space-around">
-          {/* start filters */}
-          <Col span={21}>
-            <Row>
-              <Col span={4}>
-                <Select
-                  options={options}
-                  placeholder="Select Incident type"
-                  style={{ width: 250 }}
-                />
-              </Col>
-            </Row>
-          </Col>
-          {/* end filters */}
-          {/* start actions */}
-          <Col span={3}>
-            <Row>
-              <Col span={12}>
-                <Button icon="plus" type="primary">
-                  New Plan
-                </Button>
-              </Col>
-              <Col span={12}>
-                <Dropdown overlay={menu}>
-                  <Button style={{ marginLeft: 8 }} type="primary">
-                    <Icon type="export" />
-                    Export
-                    <Icon type="down" />
+export default class PlanList extends Component {
+  state = { showForm: false };
+
+  showPlanForm = () => {
+    this.setState({ showForm: true });
+  };
+
+  hidePlanForm = () => {
+    this.setState({ showForm: false });
+  };
+
+  render() {
+    const { showForm } = this.state;
+    return (
+      <Layout>
+        {/* start primary header */}
+        <Header
+          style={{
+            background: '#fff',
+            paddingLeft: 10,
+            borderBottom: '1px solid #E0E0E0',
+            flexBasis: '50px',
+          }}
+        >
+          <h3>Plans</h3>
+        </Header>
+        {/* end primary header */}
+        {/* Toolbar */}
+        <Header
+          style={{
+            background: '#fff',
+            padding: '0 10px',
+            borderBottom: '1px solid #E0E0E0',
+            flexBasis: '50px',
+          }}
+        >
+          <Row justify="space-around">
+            {/* start filters */}
+            <Col span={22}>
+              <Row>
+                <Col span={4}>
+                  <Select
+                    options={options}
+                    placeholder="Select Incident type"
+                    style={{ width: 250 }}
+                  />
+                </Col>
+              </Row>
+            </Col>
+            {/* end filters */}
+            {/* start actions */}
+            <Col span={1}>
+              <Row justify="end">
+                <Col span={12}>
+                  <Button
+                    icon="plus"
+                    type="primary"
+                    onClick={this.showPlanForm}
+                  >
+                    New Plan
                   </Button>
-                </Dropdown>
-              </Col>
-            </Row>
-          </Col>
-          {/* end actions */}
-        </Row>
-      </Header>
-      {/* end secondary header */}
-      {/* end Toolbar */}
-      {/* Plan list content */}
-      <Content style={{ backgroundColor: '#fff' }}>
-        <div style={{ margin: '15px 15px' }}>
-          <List
-            grid={{ gutter: 10, xs: 1, sm: 2, md: 3, lg: 4, xl: 4, xxl: 4 }}
-            dataSource={fakeData}
-            renderItem={item => (
-              <List.Item>
-                <PlanCard {...item} />
-              </List.Item>
-            )}
-          />
-        </div>
-      </Content>
-      {/* end Plan list content */}
-    </Layout>
-  );
+                </Col>
+              </Row>
+            </Col>
+            {/* end actions */}
+          </Row>
+        </Header>
+        {/* end Toolbar */}
+        {/* Plan list content */}
+        <Content style={{ backgroundColor: '#fff' }}>
+          <div style={{ margin: '15px 15px' }}>
+            <List
+              grid={{ gutter: 10, xs: 1, sm: 2, md: 3, lg: 4, xl: 4, xxl: 4 }}
+              dataSource={fakeData}
+              renderItem={item => (
+                <List.Item>
+                  <PlanCard {...item} />
+                </List.Item>
+              )}
+            />
+          </div>
+        </Content>
+        {/* end Plan list content */}
+        {/* drawer for showing plan form */}
+        <Drawer
+          title="Create New Plan"
+          placement="right"
+          width="35%"
+          onClose={this.hidePlanForm}
+          maskClosable={false}
+          visible={showForm}
+        >
+          {/* plan form */}
+          <PlanForm onCancel={this.hidePlanForm} />
+          {/* end plan form */}
+        </Drawer>
+        {/* end drawer for showing plan form */}
+      </Layout>
+    );
+  }
 }
