1ae1448edeaff68378d2cb641fc968786c54eadf
mkumbobeaty <charsbeaty@gmail.com>
2018-11-20 17:19:13 +0300

    Incidents: Show popup when item is drawn

---
+++ src/dashboard/Incidents/components/IncidentForm/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/**
+ * IncidentForm component
+ * this component displays  form
+ * for creating a new incident
+ *
+ * @function
+ * @name IncidentForm
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+export default function IncidentForm () {
+
+    return (
+        <div>
+            Incident Form works
+        </div>
+    );
+}
--- src/dashboard/Incidents/index.js
+++ src/dashboard/Incidents/index.js
@@ -3,12 +3,13 @@ import { get } from 'lodash';
 import L from 'leaflet';
 import * as ReactLeaflet from 'react-leaflet';
 import 'leaflet-draw';
+import IncidentForm from './components/IncidentForm';
 import MapNav from './components/MapNav';
 
 import '../styles.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
 
-const { Map: LeafletMap, TileLayer } = ReactLeaflet;
+const { Map: LeafletMap, TileLayer, Popup } = ReactLeaflet;
 
 /**
  * Incidents component
@@ -28,6 +29,7 @@ export default class Incidents extends React.Component {
       position: [-6.8161, 39.2804],
       area: {},
       zoom: 13,
+      showPopup: false
     };
 
     this.mapRef = React.createRef();
@@ -48,7 +50,7 @@ export default class Incidents extends React.Component {
         featureGroup: this.drawnItems,
       },
     });
-  this.map.addControl(this.drawControl);
+    this.map.addControl(this.drawControl);
   }
 
   showDrawnItems = (e) => {
@@ -57,33 +59,41 @@ export default class Incidents extends React.Component {
     console.log('drwan area');
     console.log(layer.toGeoJSON());
     const type = get(layer.toGeoJSON(), 'geometry.type')
-    this.setState({ position: type === 'Point' ? layer.getLatLng() :  layer.getBounds().getCenter(), area: layer.toGeoJSON() });
+    this.setState({
+      position: type === 'Point' ? layer.getLatLng() : layer.getBounds().
+        getCenter(), area: layer.toGeoJSON(),
+      showPopup: true
+    });
   }
 
   componentDidMount() {
     this.map = this.mapRef.current.leafletElement;
     this.initDrawControls();
     this.map.on('draw:created', e => this.showDrawnItems(e));
-  
+
 
   }
 
   render() {
-    const { position, zoom } = this.state;
+    const { position, zoom, showPopup } = this.state;
     return (
       <div>
         <MapNav />
         <LeafletMap
-         center={position}
+          center={position}
           zoom={zoom}
           ref={this.mapRef}
-          >
+        >
           <TileLayer
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             id="mapbox.light"
             url="https://api.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoid29ybGRiYW5rLWVkdWNhdGlvbiIsImEiOiJIZ2VvODFjIn0.TDw5VdwGavwEsch53sAVxA#1.6/23.725906/-39.714135/0"
           />
+          {showPopup ? <Popup position={position} minWidth={400}>
+            <IncidentForm />
+          </Popup> : null}
         </LeafletMap>
+
       </div>
     );
   }
