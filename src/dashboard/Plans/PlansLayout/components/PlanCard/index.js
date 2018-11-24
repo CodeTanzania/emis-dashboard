163a3f5bd2413370649661458af6177afd20ab99
Benson Maruchu <benmaruchu@gmail.com>
2018-11-24 11:59:59 +0300

    PlanCard: Add actions for a plan

--- src/dashboard/Plans/PlansLayout/components/PlanCard/index.js
+++ src/dashboard/Plans/PlansLayout/components/PlanCard/index.js
@@ -1,8 +1,35 @@
-import { Badge, Card, Col, Row } from 'antd';
+import { Badge, Button, Card, Col, Popover, Row } from 'antd';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
+
+function PlanOptions({ onEditPlan }) {
+  return (
+    <Fragment>
+      <div>
+        <Button icon="poweroff" className="b-0">
+          Activate
+        </Button>
+      </div>
+      <div>
+        <Button icon="sound" className="b-0">
+          Disseminate
+        </Button>
+      </div>
+      <div>
+        <Button icon="edit" className="b-0" onClick={onEditPlan}>
+          Edit Plan
+        </Button>
+      </div>
+      <div>
+        <Button icon="hdd" className="b-0">
+          Archive Plan
+        </Button>
+      </div>
+    </Fragment>
+  );
+}
 
 /**
  * Plan card component
@@ -26,75 +53,174 @@ import './styles.css';
  * @version 0.1.0
  * @since 0.1.0
  */
-export default function PlanCard({
-  incidentType,
-  jurisdiction,
-  description,
-  nature,
-  family,
-  activityCount,
-  color,
-  updatedAt,
-  onClickPlan,
-}) {
-  return (
-    <Link to="/plans/plan/activities" title={description} onClick={onClickPlan}>
+export default class PlanCard extends Component {
+  state = { showPopover: false };
+
+  static propTypes = {
+    incidentType: PropTypes.string.isRequired,
+    jurisdiction: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    nature: PropTypes.string,
+    family: PropTypes.string,
+    activityCount: PropTypes.number,
+    color: PropTypes.string,
+    updatedAt: PropTypes.string.isRequired,
+    onClickPlan: PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    activityCount: 0,
+    color: '#0071fc',
+    nature: 'N/A',
+    family: 'N/A',
+    description: '',
+  };
+
+  /**
+   * Handle popover visibility change
+   *
+   * @function
+   * @name handlePopoverVisibilityChange
+   *
+   * @param {boolean} isVisible
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handlePopoverVisibilityChange = isVisible => {
+    this.setState({ showPopover: isVisible });
+  };
+
+  /**
+   * Handle hide popover component
+   *
+   * @function
+   * @name handleHidePopover
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handleHidePopover = () => {
+    this.setState({ showPopover: false });
+  };
+
+  /**
+   * Handle edit plan action
+   *
+   * @function
+   * @name handleEditPlan
+   *
+   * @param {Event} e - Event triggered object
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handleEditPlan = e => {
+    e.preventDefault();
+    this.handleHidePopover();
+  };
+
+  /**
+   * Handle disseminate plan action
+   *
+   * @function
+   * @name handleDisseminatePlan
+   *
+   * @param {Event} e - Event triggered object
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handleDisseminatePlan = e => {
+    e.preventDefault();
+    this.handleHidePopover();
+  };
+
+  /**
+   * Handle activate plan action
+   *
+   * @function
+   * @name handleActivatePlan
+   *
+   * @param {Event} e - Event triggered object
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handleActivatePlan = e => {
+    e.preventDefault();
+    this.handleHidePopover();
+  };
+
+  render() {
+    const { showPopover } = this.state;
+
+    const {
+      incidentType,
+      jurisdiction,
+      description,
+      nature,
+      family,
+      activityCount,
+      color,
+      updatedAt,
+      onClickPlan,
+    } = this.props;
+
+    return (
       <Card
         className="PlanCard"
         style={{
           borderLeft: `3px solid ${color}`,
         }}
       >
-        <Row justify="space-between">
-          <Col span={21} xl={18} xxl={20}>
-            <h3 title={incidentType}>{incidentType}</h3>
-            <p
-              className="subtitle"
-              style={{ fontSize: '11px', color: '#0092df' }}
-            >
-              {`${nature} > ${family}`}
-            </p>
-            <p className="subtitle">{jurisdiction}</p>
-          </Col>
-          <Col span={3} xl={6} xxl={4} className="activitiesBadge">
-            <Badge
-              count={activityCount}
-              style={{
-                backgroundColor: '#fff',
-                color: '#999',
-                boxShadow: '0 0 0 1px #d9d9d9 inset',
-              }}
+        <Link
+          to="/plans/plan/activities"
+          title={description}
+          onClick={onClickPlan}
+        >
+          <Row justify="space-between">
+            <Col span={21} xl={18} xxl={20}>
+              <h3 title={incidentType}>{incidentType}</h3>
+              <p
+                className="subtitle"
+                style={{ fontSize: '11px', color: '#0092df' }}
+              >
+                {`${nature} > ${family}`}
+              </p>
+              <p className="subtitle">{jurisdiction}</p>
+            </Col>
+            <Col span={3} xl={6} xxl={4} className="activitiesBadge">
+              <Badge
+                count={activityCount}
+                style={{
+                  backgroundColor: '#fff',
+                  color: '#999',
+                  boxShadow: '0 0 0 1px #d9d9d9 inset',
+                }}
+              />
+              <p className="activitiesBadgeTitle">Activities</p>
+            </Col>
+          </Row>
+          <p className="subtitle">
+            Last Review Date:{' '}
+            {new Intl.DateTimeFormat('en-GB').format(new Date(updatedAt))}
+          </p>
+          <Popover
+            content={<PlanOptions onEditPlan={this.handleEditPlan} />}
+            trigger="hover"
+            placement="bottom"
+            visible={showPopover}
+            onVisibleChange={this.handlePopoverVisibilityChange}
+          >
+            <Button
+              icon="ellipsis"
+              className="ActionButton"
+              onClick={e => e.preventDefault()}
             />
-            <p className="activitiesBadgeTitle">Activities</p>
-          </Col>
-        </Row>
-        <p className="subtitle">
-          Last Review Date:{' '}
-          {new Intl.DateTimeFormat('en-GB').format(new Date(updatedAt))}
-        </p>
+          </Popover>
+        </Link>
       </Card>
-    </Link>
-  );
+    );
+  }
 }
-
-/* default Plan props */
-PlanCard.defaultProps = {
-  activityCount: 0,
-  color: '#0071fc',
-  nature: 'N/A',
-  family: 'N/A',
-  description: '',
-};
-
-/* props validations for Plan */
-PlanCard.propTypes = {
-  incidentType: PropTypes.string.isRequired,
-  jurisdiction: PropTypes.string.isRequired,
-  description: PropTypes.string,
-  nature: PropTypes.string,
-  family: PropTypes.string,
-  activityCount: PropTypes.number,
-  color: PropTypes.string,
-  updatedAt: PropTypes.string.isRequired,
-  onClickPlan: PropTypes.func.isRequired,
-};
