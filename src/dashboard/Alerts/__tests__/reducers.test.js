de8b552b7681932f3103ec548509749c3a0ab9f8
mloweedgar <mloweedgar0@gmail.com>
2018-12-24 23:57:09 +0300

    Add tests for reducers which store alerts to state

--- src/dashboard/Alerts/__tests__/reducers.test.js
+++ src/dashboard/Alerts/__tests__/reducers.test.js
@@ -1,3 +1,4 @@
+import * as Actions from '../actions';
 import { alertsMap, alerts } from '../reducers';
 
 describe('Alerts:reducers', () => {
@@ -15,6 +16,14 @@ describe('Alerts:reducers', () => {
 
     it('should return default state when no initial alertsMap state is provided', () => {
       expect(alertsMap(undefined, {})).toEqual(previousState);
+    });
+
+    it('should return previous state when given invalid action type', () => {
+      expect(
+        alertsMap(previousState, {
+          type: null,
+        })
+      ).toEqual(previousState);
     });
   });
 
@@ -36,6 +45,72 @@ describe('Alerts:reducers', () => {
 
     it('should return default state when no initial alert state is provided', () => {
       expect(alerts(undefined, {})).toEqual(previousState);
+    });
+
+    it('should return previous state when given invalid action type', () => {
+      expect(
+        alerts(previousState, {
+          type: null,
+        })
+      ).toEqual(previousState);
+    });
+
+    it(`should handle ${Actions.GET_ALERTS_START}`, () => {
+      const nextState = {
+        ...previousState,
+        loading: true,
+      };
+      expect(alerts(previousState, { type: Actions.GET_ALERTS_START })).toEqual(
+        nextState
+      );
+    });
+
+    it(`should handle ${Actions.GET_ALERTS_SUCCESS}`, () => {
+      const action = {
+        type: Actions.GET_ALERTS_SUCCESS,
+        payload: {
+          data: [],
+        },
+        meta: {
+          page: 2,
+          total: 200,
+        },
+      };
+
+      const nextState = {
+        ...previousState,
+        data: [...action.payload.data],
+        page: action.meta.page,
+        total: action.meta.total,
+      };
+
+      expect(alerts(previousState, action)).toEqual(nextState);
+    });
+
+    it(`should handle ${Actions.GET_ALERTS_ERROR}`, () => {
+      const error = {
+        status: 404,
+        code: 404,
+        name: 'Error',
+        message: 'Not Found',
+        developerMessage: 'Not Found',
+        userMessage: 'Not Found',
+        error: 'Error',
+        error_description: 'Not Found',
+      };
+
+      const action = {
+        type: Actions.GET_ALERTS_ERROR,
+        payload: { data: error },
+        error: true,
+      };
+
+      const expectedState = {
+        ...previousState,
+        error: action.payload.data,
+      };
+
+      expect(alerts(previousState, action)).toEqual(expectedState);
     });
   });
 });
