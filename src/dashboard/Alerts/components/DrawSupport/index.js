a21d47fcdb3b9acc729218f994a026fa2b01a8db
mloweedgar <mloweedgar0@gmail.com>
2018-12-24 16:00:02 +0300

    Alerts: Display alerts as map markers

--- src/dashboard/Alerts/components/DrawSupport/index.js
+++ src/dashboard/Alerts/components/DrawSupport/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import L from 'leaflet';
 
 class DrawSupport extends React.Component {
   componentDidMount() {
-    const { map } = this.props;
-    L.control.zoom({ position: 'bottomleft' }).addTo(map);
+    this.map = this.props.map;
+    L.control.zoom({ position: 'bottomleft' }).addTo(this.map);
   }
+
+  componentDidUpdate(prevProps) {
+    const { points } = this.props;
+    const { points: prevPoints } = prevProps;
+
+    if (points !== prevPoints) {
+      this.showPoints(points);
+    }
+  }
+
+  showPoints = points => {
+    const DefaultIcon = L.icon({
+      iconUrl: icon,
+      shadowUrl: iconShadow,
+    });
+    L.Marker.prototype.options.icon = DefaultIcon;
+    this.pointsLayer = L.geoJSON().addTo(this.map);
+    this.pointsLayer.addData(points);
+  };
 
   render() {
     return null;
   }
 }
 
-export default DrawSupport;
+const mapStateToProps = state => ({
+  points: state.alertsMap.points,
+});
+
+export default connect(mapStateToProps)(DrawSupport);
