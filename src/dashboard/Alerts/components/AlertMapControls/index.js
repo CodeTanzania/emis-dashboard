20004140f4a7f1819f3c2ebf70b07473d9088f03
mloweedgar <mloweedgar0@gmail.com>
2019-01-03 11:41:18 +0300

    Add a tutorial for guiding users during Alert creation

--- src/dashboard/Alerts/components/AlertMapControls/index.js
+++ src/dashboard/Alerts/components/AlertMapControls/index.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withLeaflet } from 'react-leaflet';
+import L from 'leaflet';
 import PropTypes from 'prop-types';
 import { saveDrawnGeometryOperation } from '../../actions';
 import DrawControls from '../../../../common/components/DrawControls';
 
+const popupContent = `<div>
+<div class="ant-modal-body">
+    <h2>To create new Alert, Draw the area that is involved  in the Alert using the Draw Controls and Fill the Form</h2>
+</div>
+<div class="ant-modal-footer">
+    <div>
+        <button type="button" id="info-button" class="ant-btn ant-btn-primary"><span>OK</span></button>
+    </div>
+</div>
+</div>`;
+
 class AlertMapControls extends React.Component {
+  static propTypes = {
+    saveGeometry: PropTypes.func,
+    leaflet: PropTypes.shape({
+      map: PropTypes.object.isRequired,
+    }).isRequired,
+  };
+
+  static defaultProps = {
+    saveGeometry: () => {},
+  };
+
   constructor(props) {
     super(props);
     this.onDrawCreated = this.onDrawCreated.bind(this);
   }
+
+  componentDidMount() {
+    this.map = this.props.leaflet.map;
+    this.showTutorial();
+  }
+
+  showTutorial = () => {
+    L.popup({ minWidth: 450 })
+      .setLatLng([-6.179, 35.754])
+      .setContent(popupContent)
+      .openOn(this.map);
+    document.querySelector('#info-button').addEventListener('click', e => {
+      e.preventDefault();
+      this.map.closePopup();
+    });
+  };
 
   onDrawCreated = ({ geometry }) => {
     const { saveGeometry } = this.props;
@@ -26,12 +66,4 @@ const mapDispatchToProps = {
 export default connect(
   null,
   mapDispatchToProps
-)(AlertMapControls);
-
-AlertMapControls.propTypes = {
-  saveGeometry: PropTypes.func,
-};
-
-AlertMapControls.defaultProps = {
-  saveGeometry: () => {},
-};
+)(withLeaflet(AlertMapControls));
